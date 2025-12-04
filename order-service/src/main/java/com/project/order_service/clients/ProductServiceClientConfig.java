37f463a43a1d4e5db54ef487b2572b4281c70314
i-amDev <92856531+i-amDev@users.noreply.github.com>
2025-12-04 10:24:52 +0530

    - Added the "defaultStatusHandler".

--- order-service/src/main/java/com/project/order_service/clients/ProductServiceClientConfig.java
+++ order-service/src/main/java/com/project/order_service/clients/ProductServiceClientConfig.java
@@ -3,9 +3,12 @@ package com.project.order_service.clients;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import java.util.Optional;
 
 @Configuration
 public class ProductServiceClientConfig {
@@ -18,7 +21,10 @@ public class ProductServiceClientConfig {
 
     @Bean
     public ProductServiceClient restClientInterface(RestClient.Builder restClientBuilder) {
-        RestClient restClient = restClientBuilder.baseUrl("http://product-service").build();
+        RestClient restClient = restClientBuilder
+                .baseUrl("http://product-service")
+                .defaultStatusHandler(HttpStatusCode::is4xxClientError, (request, response) -> Optional.empty())
+                .build();
         RestClientAdapter adapter = RestClientAdapter.create(restClient);
         HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(adapter).build();
         ProductServiceClient productServiceClient = factory.createClient(ProductServiceClient.class);
