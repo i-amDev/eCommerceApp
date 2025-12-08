b073a6de4e168337d25c0fb4e8cf2a44d8e63945
i-amDev <92856531+i-amDev@users.noreply.github.com>
2025-12-08 11:49:53 +0530

    - Implemented the "customRouteLocator" method to write the configuration in ".java" file for "product-service".

--- api-gateway/src/main/java/com/project/api_gateway/config/GatewayConfig.java
+++ api-gateway/src/main/java/com/project/api_gateway/config/GatewayConfig.java
@@ -1,9 +1,20 @@
 package com.project.api_gateway.config;
 
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class GatewayConfig {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder routeLocatorBuilder) {
+        return routeLocatorBuilder.routes()
+                .route("product-service", r -> r.path("/product/**")
+                        .uri("lb://PRODUCT-SERVICE"))
+                .build();
+    }
 
 
 }
